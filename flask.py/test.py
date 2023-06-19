1bd4c0e4a2f461039d7774007ad629676dd6ff60
Aaron Frazier Jr <afraz91@wgu.edu>
2023-06-19 13:01:20 -0700

    I added a new line to flask file

--- flask.py/test.py
+++ flask.py/test.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def home():
     return "Hello, World! This is a test for HTML home page <h1>HELLO</h1>"
 
+def user(name):
+    return f"Hello {name}!"
+
 if __name__ == '__main__':
     app.run()
